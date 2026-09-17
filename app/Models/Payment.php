8c29cebabb74096569a9c5ee35488725618ff4f0
dev <yassineettai120@gmail.com>
2026-09-17 17:31:24 +0100

    Add payment controller

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Payement extends Model
+class Payment extends Model
 {
-    /** @use HasFactory<\Database\Factories\PayementsFactory> */
-    use HasFactory;
+    protected $fillable = [
+        'transaction_id',
+        'amount',
+        'payment_method',
+        'status',
+        'paid_at',
+        'transaction_reference',
+    ];
 }
